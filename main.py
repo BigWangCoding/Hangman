079630a5c53124157eff9717f86e0fe655a6fc13
Andrew Wang <andrewwang508@gmail.com>
2023-09-17 21:46:01 -0500

    Added a letter box for user to type in a letter
    The letter box will be will the user enters a letter, if the user presses a different letter, the letter will be replaced with the new letter. This required us to import a letter box drawing

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
 FPS = 60
 
-SCALED_WIDTH, SCALED_HEIGHT = WIDTH * 0.7, HEIGHT * 0.7
+SCALED_WIDTH, SCALED_HEIGHT = WIDTH * 0.4, HEIGHT * 0.6
 
+LETTER_SIZE_WIDTH = 175
+LETTER_SIZE_HEIGHT = 175
+
+LETTER_BOX = pygame.image.load(
+    os.path.join("Assets", "Enter-letter-box.png"))
+LETTER_BOX = pygame.transform.scale(LETTER_BOX, (LETTER_SIZE_WIDTH, LETTER_SIZE_HEIGHT))
+LETTER_BOX_X_OFFSET, LETTER_BOX_Y_OFFSET = 10, 10
 # Getting hangman image from assets folder
 HANG_MAN_IMAGE = pygame.image.load(
     os.path.join("Assets", "Hangman.png"))
@@ -18,20 +25,26 @@ HANG_MAN_IMAGE = pygame.image.load(
 HANG_MAN = pygame.transform.scale(HANG_MAN_IMAGE, (SCALED_WIDTH, SCALED_HEIGHT))
 pygame.display.set_caption("Hangman")
 
-SECRET_WORD = "How, are, you, doing"
+SECRET_WORD = "Hello, my name is andy"
 WORDS = SECRET_WORD.split()
 
 FONT = "Comic Sans MS"
 TEXT_SIZE = 50
-#DashPosition = set()
+
 # RGB colors and have to update the display to actually change colors
 
-size = SCALED_HEIGHT / len(SECRET_WORD) - 10
-lineSize = size if size > 25 else 25
+
+lineSize = 25
+
+PositionsOfLetters = {}
+def draw_letter_box():
+    WINDOW.blit(LETTER_BOX, (LETTER_BOX_X_OFFSET, LETTER_BOX_Y_OFFSET))
 def draw_window():
     WINDOW.fill(WHITE)
     # Drawing hangman image into screen
-    WINDOW.blit(HANG_MAN, (0, 0))
+    draw_letter_box()
+    WINDOW.blit(HANG_MAN, (100, 0))
+    
 
     
     
@@ -39,6 +52,7 @@ def draw_window():
 
     positionY = 100
     curWord = 1
+    prevLetter = None
     for i in SECRET_WORD:
         
         if i == " ":
@@ -47,37 +61,74 @@ def draw_window():
             totalSpaceTaken = len(word) * lineSize + newPositionX
             
             
-            if WIDTH - totalSpaceTaken < 0:
+            if WIDTH - totalSpaceTaken < 50:
                 positionY += 50
                 prevPositionX, newPositionX = 500, 500 + lineSize
-                continue
+                prevLetter = i
+            elif prevLetter == ",":
+                pass    
+            else:
+                prevPositionX, newPositionX = newPositionX + 10, newPositionX + lineSize + 10
+            prevLetter = i
+            continue
+
+            
         elif i == ",":
             text = pygame.font.SysFont(FONT, TEXT_SIZE)
             text = text.render(",", False, (0,0,0))
             WINDOW.blit(text, (prevPositionX - 10, positionY - 50))
+
         else: 
-            newLine = pygame.draw.line(WINDOW, (0,0,0), (prevPositionX, positionY), (newPositionX, positionY), 4)
-            #DashPosition.add((prevPositionX, newPositionX, positionY))
+            startPoint = (prevPositionX, positionY)
+            endPoint = (newPositionX, positionY)
+            PositionsOfLetters[(startPoint, endPoint)] = i
+            pygame.draw.line(WINDOW, (0,0,0), startPoint, endPoint, 4)
+
         prevPositionX, newPositionX = newPositionX + 10, newPositionX + lineSize + 10
+        prevLetter = i
         
     pygame.display.update()
+def insertLetter(letter):
+    letter = letter.title()
+    text = pygame.font.SysFont(FONT, TEXT_SIZE * 2)
+    text = text.render(letter, False, (0,0,0))
+    half = TEXT_SIZE/2
+    centerX = (LETTER_SIZE_WIDTH - LETTER_BOX_X_OFFSET)/2 - half
+    centerY = (LETTER_SIZE_HEIGHT - LETTER_BOX_Y_OFFSET)/2 - TEXT_SIZE
+
+    
+
+    WINDOW.blit(text, (centerX, centerY))
+    pygame.display.update()
+    
 
 # The function main will be where we run the game
 def main():
     clock = pygame.time.Clock()
     run = True
+    draw_window()
     # To run pygame, you must have to check if the user has quit the screen
     # pygame.QUIT tells us that the user has exited out of the window
     while run:
         clock.tick(FPS)
+
+        key = None
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
-         
+            elif event.type == pygame.KEYDOWN:
+                key = pygame.key.name(event.key)
+                if not key.isalpha():
+                    key = None
+                
+        
+
+        if key:
+            draw_letter_box()
+            insertLetter(key)
+                
     pygame.quit()
-
-
 
 # Running main function only if the current file name is called main
 # cannot run main from another file
